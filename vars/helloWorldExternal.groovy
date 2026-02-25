bac012f11ff7e562969ed7747529d27f56731b03
sorinrepository <sorinmiulescu@gmail.com>
2026-02-25 10:49:40 +0200

    Fix script path in helloWorldExternal.groovy

--- vars/helloWorldExternal.groovy
+++ vars/helloWorldExternal.groovy
@@ -2,5 +2,5 @@ def call(Map config = [:]) {
     loadlinuxscript(name : 'hello-world.sh')
     sh "pwd"
     sh "ls -al"
-    sh "/home/jenkins/workspace/firstpipeline_declarative/hello-world.sh ${config.name} ${config.day}"
+    sh "hello-world.sh ${config.name} ${config.day}"
 }
