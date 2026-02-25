c2dbbd1fdeff5850593f4246d4cebe1c5b388eaf
Sorin <sorin.miuelescu@vodafone.com>
2026-02-25 10:04:28 +0200

    Update helloWorldExternal.groovy

--- vars/helloWorldExternal.groovy
+++ vars/helloWorldExternal.groovy
@@ -1,4 +1,4 @@
-def helloWorld(Map config = [:]) {
+def helloWorldExternal(Map config = [:]) {
     loadlinuxscript(name : "hello-world.sh")
     sh './hello-world.sh ${config.name} ${config.day}.'
 }
