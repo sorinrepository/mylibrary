fb83d281c235539b159a245eacac03234332f130
sorinrepository <sorinmiulescu@gmail.com>
2026-02-25 10:33:41 +0200

    Fix string quotes in helloWorldExternal.groovy

--- vars/helloWorldExternal.groovy
+++ vars/helloWorldExternal.groovy
@@ -1,4 +1,4 @@
 def call(Map config = [:]) {
     loadlinuxscript(name : "hello-world.sh")
-    sh './hello-world.sh ${config.name} ${config.day}'
+    sh "./hello-world.sh ${config.name} ${config.day}"
 }
