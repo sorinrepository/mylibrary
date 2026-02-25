bf55609d081de51d770b6db08b9625cf1f170414
sorinrepository <sorinmiulescu@gmail.com>
2026-02-25 10:40:46 +0200

    Update loadlinuxscript.groovy

--- vars/loadlinuxscript.groovy
+++ vars/loadlinuxscript.groovy
@@ -1,6 +1,7 @@
 def call(Map config = [:]) {
     def scriptcontent = libraryResource "sorin/linux/${config.name}"
     writeFile file: "${config.name}", text: scriptcontent
-    sh "pwd || ls"
+    sh "pwd"
+    sh "ls -al"
     sh "chmod a+x ./${config.name}"
 }
